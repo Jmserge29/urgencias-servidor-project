a9f7434bbe4eadb650b9e6d79e62604ebc0b5f21
Jmserge29 <Jmserge29@gmail.com>
2023-10-09 21:16:12 -0500

    Update errors in the server 2

--- Controllers/doctor.controller.js
+++ Controllers/doctor.controller.js
@@ -5,16 +5,15 @@ const signInDoctor = async (req, res) => {
   try {
     const { correo, password } = req.body;
     // Buscar al doctor por su email en la base de datos
-    console.log("hey");
     const doctor = await Doctor.findOne({ correo });
-
+    
     if (!doctor) {
-      return res.status(404).json({ error: "Credenciales Incorrectas" });
+        return res.status(404).json({ error: "Credenciales Incorrectas" });
     }
-
+    
     // Verificar la contraseña ingresada con la contraseña almacenada en la base de datos
-    const isPasswordValid = await Doctor.comparePassword(password);
-
+    const isPasswordValid = await Doctor.comparePassword(password, doctor.password);
+    
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Unauthorized!" });
     }
@@ -25,7 +24,7 @@ const signInDoctor = async (req, res) => {
     });
 
     // Enviar el token en la respuesta
-    return res.status(200).json({ token });
+    return res.status(200).json({ token , doctor});
   } catch (error) {
     console.error("Error en el inicio de sesión:", error);
     return res.status(500).json({ error: "Error interno del servidor." });
